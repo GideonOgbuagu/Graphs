a6859c31f9b8b8ee9db0b1ea0183f3ecda06c7d7
GideobOgbuagu <reachgiddy@gmail.com>
2020-11-17 20:58:22 -0500

    updated solutions for graph.py & ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,79 @@
+"""
+Understand
+    1   2
+     \ /
+      3
+       \
+        6
+input: [(1, 3), (2, 3), (3, 6)]
+
+starting node: 6
+output: 1 (1 and 2 are tied but 1 has a lower id)
+
+starting node: 1
+output: -1 (1 has no ancestor)
+
+Plan
+1. Translate the problem into graph terminology
+vertex - a person (in this case, we're given their ids)
+edge - parent-child relationship between two people
+path - a person's family tree
+weight - not needed, all edges are equal and have no value/cost related to them
+
+2. Build your graph
+Build a graph by using the parent-child relationships/edges we're given. Each node in the
+graph will have an outgoing/directed edge to its parent/ancestor.
+
+3. Traverse the graph
+We traverse the graph while keeping track of the node's distances from the starting node
+and keep track of the terminal node with the lowest id and greatest distance.
+A terminal node will have no outgoing edges, meaning it has no more ancestors. A terminal node
+doesn't mean it's the earliest ancestor though, so we need to consider the terminal node that is
+the greatest distance from the starting node (that also has the lowest id).
+
+In this case, we can use a depth-first traversal (DFT) to traverse all of the starting node's
+ancestors and return the earliest one with the lowest id.
+"""
+
+from collections import deque
+from collections import defaultdict
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+    graph = createGraph(ancestors)
+    # A tuple with a node and its distance from the starting node
+    # At the beginning, the starting node's earliest ancestor is itself
+    earliestAncestor = ((starting_node, 0))
+    stack = deque()
+    stack.append((starting_node, 0))
+    visited = set()
+    while len(stack) > 0:
+        curr = stack.pop()
+        currNode, distance = curr[0], curr[1]
+        visited.add(curr)
+
+        # This checks if the node is a terminal node
+        if currNode not in graph:
+            # Only consider terminal nodes that have a greater distance than the ones we've found so far
+            if distance > earliestAncestor[1]:
+                earliestAncestor = curr
+            # if  there's a tie then the ancestor with the lower id
+            elif distance == earliestAncestor[1] and currNode < earliestAncestor[0]:
+                earliestAncestor = curr
+        
+        else:
+            for ancestor in graph[currNode]:
+                if ancestor not in visited:
+                    stack.append((ancestor, distance + 1))
+            
+    # if the starting node's earliest ancestor is itself, then just return -1
+    return earliestAncestor[0] if earliestAncestor[0] != starting_node else -1
+
+# Creates a graph where the keys are a node and its values are its ancestors
+def createGraph(edges):
+    graph = defaultdict(set)
+    for edge in edges:
+        ancestor, child = edge[0], edge[1]
+        graph[child].add(ancestor)
+    return graph
+
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,7 +1,7 @@
 """
 Simple graph implementation
 """
-from util import Stack, Queue  # These may come in handy
+from collections import deque
 
 
 class Graph:
@@ -11,22 +11,36 @@ class Graph:
     def __init__(self):
         self.vertices = {}
 
+    def __repr__(self):
+        return str(self.vertices)
+
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        # pass  # TODO
-        # if not self.vertices[vertex_id]:
         self.vertices[vertex_id] = set()
-
+      
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        # pass  # TODO
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-            # self.vertices[v2].add(v1)
+        if v1 not in self.vertices or v2 not in self.vertices:
+            print("Attempting to add edge to non-existing nodes")
+        self.vertices[v1].add(v2)
+       
+    def remove_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            print("Attempting to remove non-existent vertex")
+            return
+        self.vertices.pop(vertex_id)
+        for remaining_vertex in self.vertices:
+            self.vertices[remaining_vertex].discard(vertex_id)
+
+    def remove_edge(self, from_vertex_id, to_vertex_id):
+        if from_vertex_id not in self.vertices or to_vertex_id not in self.vertices:
+            print("Attempting to remove edges from non-existent vertex")
+            return
+        self.vertices[from_vertex_id].discard(to_vertex_id)
 
     def get_neighbors(self, vertex_id):
         """
@@ -41,25 +55,21 @@ class Graph:
         beginning from starting_vertex.
         """
         # pass  # TODO
-        q = Queue()
-        q.enqueue(starting_vertex)
-
         visited = set()
-
+        queue = deque()
+        queue.append(starting_vertex)
         # while the stack isn't empty
-        while q.size() > 0:
+        while len(queue) > 0:
             # pop off top of stack, this is our current node
-            v = q.dequeue()
-        # if we have not visited, then let's:
-            if v not in visited:
+            currNode = queue.popleft()
+            # if we have not visited, then let's:
+            if currNode not in visited:
                 # mark as visited
-                visited.add(v)
+                visited.add(currNode)
 
-        # get the vertex's neighbors
-                neighbors = self.get_neighbors(v)
-        # put the current node's neighbors on the stack
-                for neighbor in neighbors:
-                    q.enqueue(neighbor)
+                # put the current node's neighbors on the stack
+                for neighbor in self.vertices[currNode]:
+                    queue.append(neighbor)
 
     def dft(self, starting_vertex):
         """
@@ -67,26 +77,21 @@ class Graph:
         beginning from starting_vertex.
         """
         pass  # TODO
-        stack = Stack()
-        stack.push(starting_vertex)
-
-        # make a set to track visited nodes
         visited = set()
-
+        stack = deque()
+        stack.append(starting_vertex)
         # while the stack isn't empty
-        while stack.size() > 0:
+        while len(stack) > 0:
             # pop off top of stack, this is our current node
-            current_node = stack.pop()
-        # if we have not visited, then let's:
-            if current_node not in visited:
+            currNode = stack.pop()
+            # if we have not visited, then let's:
+            if currNode not in visited:
                 # mark as visited
-                visited.add(current_node)
+                visited.add(currNode)
 
-        # get the vertex's neighbors
-                neighbors = self.get_neighbors(current_node)
-        # put the current node's neighbors on the stack
-                for neighbor in neighbors:
-                    stack.push(neighbor)
+                # put the current node's neighbors on the stack
+                for neighbor in self.vertices[currNode]:
+                    stack.append(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
@@ -96,6 +101,16 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+        visited = set()
+        self.dft_recursive_helper(starting_vertex, visited)
+
+    def dft_recursive_helper(self, curr_vertex, visited):
+        visited.add(curr_vertex)
+        print(curr_vertex)
+        for neighbor in self.vertices[curr_vertex]:
+            if neighbor not in visited:
+                # recursive case
+                self.dft_recursive_helper(neighbor, visited)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -104,6 +119,26 @@ class Graph:
         breath-first order.
         """
         pass  # TODO
+        """
+        Return a list containing the shortest path from
+        starting_vertex to destination_vertex in
+        breath-first order.
+        """
+        visited = set()
+        queue = deque()
+        queue.append([starting_vertex])
+        while len(queue) > 0:
+            currPath = queue.popleft()
+            currNode = currPath[-1]
+            if currNode == destination_vertex:
+                return currPath
+            if currNode not in visited:
+                visited.add(currNode)
+                for neighbor in self.vertices[currNode]:
+                    newPath = list(currPath)
+                    newPath.append(neighbor)
+                    queue.append(newPath)
+        return []
         
 
     def dfs(self, starting_vertex, destination_vertex):
@@ -113,6 +148,21 @@ class Graph:
         depth-first order.
         """
         pass  # TODO
+        visited = set()
+        stack = deque()
+        # Push the current path you're on onto the stack, instead of just a single vertex
+        stack.append([starting_vertex])
+        while len(stack) > 0:
+            currPath = stack.pop()
+            currNode = currPath[-1] # the current node you're on is the last node in the path
+            if currNode == destination_vertex:
+                return currPath
+            if currNode not in visited:
+                visited.add(currNode)
+                for neighbor in self.vertices[currNode]:
+                    newPath = list(currPath) # make a copy of the current path
+                    newPath.append(neighbor)
+                    stack.append(newPath)
 
     def dfs_recursive(self, starting_vertex, destination_vertex):
         """
@@ -123,11 +173,19 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+        # visited = set()
+        # return self.dfs_recursive_helper([starting_vertex], visited, goal_vertex)
+
+    # def dfs_recursive_helper(self, curr_path, goal_vertex):
+    #     curr_vertex = curr_path[-1]
+    #     if curr_vertex == goal_vertex:
+            
+        
 
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
-    # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
+#     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
     graph.add_vertex(1)
     graph.add_vertex(2)
     graph.add_vertex(3)
